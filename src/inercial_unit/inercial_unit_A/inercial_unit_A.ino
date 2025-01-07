ebabbbbadc19defa8ddafb8556d67e2779c3d065
JanRZ003 <zubo3@uniba.sk>
2025-01-07 21:03:47 +0100

    transformation to azimuth and simulation #43
    transformation seems correct, but certainly testing on our school telescope prototype is required

--- src/inercial_unit/inercial_unit_A/inercial_unit_A.ino
+++ src/inercial_unit/inercial_unit_A/inercial_unit_A.ino
@@ -1,5 +1,6 @@
 
 #include "gain_roll_pitch_yaw.h"
+#include "transform_to_azimuth_elevation.h"
 
 void setup() {
     Serial.begin(115200);
@@ -16,12 +17,22 @@ void loop() {
         lastUpdate = currentMillis;
 
         RollPitchYaw* roll_pitch_yaw = readFromSensor();
+        AzimuthElevation* azimuth_elevation = fromRPYtoAzimuthElevation(roll_pitch_yaw);
 
         Serial.print(F("Roll: "));
         Serial.print(roll_pitch_yaw->roll, 1);
         Serial.print(F(" Pitch: "));
         Serial.print(roll_pitch_yaw->pitch, 1);
         Serial.print(F(" Yaw: "));
-        Serial.println(roll_pitch_yaw->yaw, 1);
+        Serial.print(roll_pitch_yaw->yaw, 1);
+
+        if (std::isnan(azimuth_elevation->azimuth) || std::isnan(azimuth_elevation->elevation)) {
+            Serial.println(F(" Azimuth/Elevation: NaN"));
+        } else {
+            Serial.print(F(" Azimuth: "));
+            Serial.print(azimuth_elevation->azimuth, 1);
+            Serial.print(F(" Elevation: "));
+            Serial.println(azimuth_elevation->elevation, 1);
+        }
     }
 }

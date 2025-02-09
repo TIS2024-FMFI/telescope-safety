1b019c74527ab821f09e784993a0ccdfda7926df
JanRZ003 <zubo3@uniba.sk>
2025-02-09 13:08:30 +0100

    third sensor ism works automatically when manually put on, high accuracy

--- src/inercial_unit/inercial_unit_A/gain_roll_pitch_yaw.cpp
+++ src/inercial_unit/inercial_unit_A/gain_roll_pitch_yaw.cpp
@@ -3,15 +3,54 @@
 
 double yawOffset=0.0;
 
-uint8_t AD0_VAL = 0;
+SensorType detectedSensor=SENSOR_UNKNOWN;
 
-#ifdef USE_SPI
-ICM_20948_SPI myICM;
-#else
-ICM_20948_I2C myICM;
-#endif
+//definitions needed for icm sensors
+uint8_t AD0_VAL = 0;                  //
+                                      //
+#ifdef USE_SPI                        //
+ICM_20948_SPI myICM;                  //
+#else                                 //
+ICM_20948_I2C myICM;                  //
+#endif                                //
+//
+
+//definitions needed for ism sensor
+static unsigned long last_us = 0, now_us = 0;  //
+
+SparkFun_ISM330DHCX ISM330;                    //
+
+sfe_ism_data_t acc;                            //
+sfe_ism_data_t gyr;                            //
+
+#define CAL_GYRO                               //
+float gxyz_offsets[3] = {0};                   //
+
+float RadiansPerSecond = PI / 180000.0; //conversion factor for Mahony filter
+float Axyz[3] = {0}, Gxyz[3] = {0}; //arrays for Mahony filter
+
+float Kp = 50.0, Ki = 0.0; //PID constants for Mahony filter
+float q[4] = {1.0, 0.0, 0.0, 0.0}; //initialize quaternion, global access. X points at yaw=0
+//
+
+SensorType detectSensor() {
+    Wire.begin();
+
+    Wire.beginTransmission(0x68);
+    if (Wire.endTransmission() == 0) return SENSOR_ICM20948;
+
+    Wire.beginTransmission(0x69);
+    if (Wire.endTransmission() == 0) return SENSOR_ICM20948;
+
+    Wire.beginTransmission(0x6B);
+    if (Wire.endTransmission() == 0) return SENSOR_ISM330DHCX;
+
+    return SENSOR_UNKNOWN; // No sensor detected
+}
 
 void initializeSensor() {
+  detectedSensor = detectSensor();
+  if (detectedSensor == SENSOR_ICM20948) {
 #ifdef USE_SPI
     SPI_PORT.begin();
 #else
@@ -50,10 +89,82 @@ void initializeSensor() {
         SERIAL_PORT.println(F("Enable DMP failed!"));
         while (1);
     }
+    }
+    else if (detectedSensor == SENSOR_ISM330DHCX) {
+#if defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2_TFT)
+        pinMode(TFT_I2C_POWER, OUTPUT);
+        digitalWrite(TFT_I2C_POWER, HIGH);
+#endif
+
+        Wire.begin();
+
+        Serial.begin(115200);
+        while (!Serial);  //wait for serial monitor to connect (can replace this with delay())
+
+        if ( !ISM330.begin(0x6B) ) {  //0x6B is default with SFE library
+          Serial.println("ISM330 not detected.");
+        while (1) delay(1);
+        }
+
+  //  Serial.println("Applying settings.");
+
+  ISM330.setDeviceConfig();
+  ISM330.setBlockDataUpdate();
+
+  // Set the output data rate and precision of the accelerometer
+  ISM330.setAccelDataRate(ISM_XL_ODR_104Hz);
+  ISM330.setAccelFullScale(ISM_2g);
+
+  // Set the output data rate and precision of the gyroscope
+  ISM330.setGyroDataRate(ISM_GY_ODR_104Hz);
+  ISM330.setGyroFullScale(ISM_500dps);
+
+  /*
+  	// Turn on the accelerometer's filter and apply settings.
+  	ISM330.setAccelFilterLP2();
+  	ISM330.setAccelSlopeFilter(ISM_LP_ODR_DIV_100);
+  */
+  // Turn on the gyroscope's filter and apply settings.
+  ISM330.setGyroFilterLP1();
+  ISM330.setGyroLP1Bandwidth(ISM_MEDIUM);
+
+#ifdef CAL_GYRO
+
+#define GYRO_SAMPLES 500
+  Serial.println("Keep gyro still for offset calculation");
+  delay(2000);
+
+
+  int loopcount = 0;
+  while (loopcount < GYRO_SAMPLES) {  //accumulate sums
+    // Check if both gyroscope and accelerometer data are available.
+    if ( ISM330.checkStatus() ) {
+      ISM330.getGyro(&gyr);
+      gxyz_offsets[0] += gyr.xData;
+      gxyz_offsets[1] += gyr.yData;
+      gxyz_offsets[2] += gyr.zData;
+      loopcount++;
+    }
+  }
+
+  // calculate and store gyro offsets
+
+  for (int i = 0; i < 3; i++) gxyz_offsets[i] /= (float)loopcount;
+  #endif
+
+  // reminder!
+  Serial.print("Gyro offsets ");
+  for (int i = 0; i < 3; i++) {
+    Serial.print(gxyz_offsets[i], 1);
+    Serial.print(" ");
+  }
+  Serial.println();
+    }
 }
 
 RollPitchYaw* readFromSensor() {
-    static RollPitchYaw roll_pitch_yaw;
+  static RollPitchYaw roll_pitch_yaw;
+  if (detectedSensor == SENSOR_ICM20948) {
     icm_20948_DMP_data_t data;
 
     myICM.readDMPdataFromFIFO(&data);
@@ -93,5 +204,101 @@ RollPitchYaw* readFromSensor() {
         }
     }
 
-    return &roll_pitch_yaw;
+    }
+    else if (detectedSensor == SENSOR_ISM330DHCX) {
+      if ( ISM330.checkStatus() ) {
+        ISM330.getAccel(&acc);
+        ISM330.getGyro(&gyr);
+        now_us = micros();
+
+        float amag = sqrt(acc.xData * acc.xData + acc.yData * acc.yData + acc.zData * acc.zData);
+
+          Axyz[0] = acc.xData / amag;
+          Axyz[1] = acc.yData / amag;
+          Axyz[2] = acc.zData / amag;
+
+          Gxyz[0] = (gyr.xData - gxyz_offsets[0]) * RadiansPerSecond;
+          Gxyz[1] = (gyr.yData - gxyz_offsets[1]) * RadiansPerSecond;
+          Gxyz[2] = (gyr.zData - gxyz_offsets[2]) * RadiansPerSecond;
+
+          float deltat = (now_us - last_us) * 1.0e-6; //seconds since last update
+          last_us = now_us;
+
+          Mahony_update(Axyz[0], Axyz[1], Axyz[2], Gxyz[0], Gxyz[1], Gxyz[2], deltat);
+
+          float roll  = atan2((q[0] * q[1] + q[2] * q[3]), 0.5 - (q[1] * q[1] + q[2] * q[2]));
+          float pitch = asin(2.0 * (q[0] * q[2] - q[1] * q[3]));
+          //conventional yaw increases clockwise from North. Not that the MPU-6050 knows where North is.
+          float yaw   = -atan2((q[1] * q[2] + q[0] * q[3]), 0.5 - (q[2] * q[2] + q[3] * q[3]));
+          // to degrees
+          yaw   *= 180.0 / PI;
+          pitch *= 180.0 / PI;
+          roll *= 180.0 / PI;
+
+          roll_pitch_yaw.yaw=yaw;
+          roll_pitch_yaw.pitch=pitch;
+          roll_pitch_yaw.roll=roll;
+
+          roll_pitch_yaw.yaw += yawOffset;
+          if (roll_pitch_yaw.yaw > 180.0) roll_pitch_yaw.yaw -= 360.0;
+          if (roll_pitch_yaw.yaw < -180.0) roll_pitch_yaw.yaw += 360.0;
+
+          }
+  
+}
+return &roll_pitch_yaw;
+}
+
+void Mahony_update(float ax, float ay, float az, float gx, float gy, float gz, float deltat) {
+  float recipNorm;
+  float vx, vy, vz;
+  float ex, ey, ez;  //error terms
+  float qa, qb, qc;
+  static float ix = 0.0, iy = 0.0, iz = 0.0;  //integral feedback terms
+
+  // Estimated direction of gravity in the body frame (factor of two divided out)
+  vx = q[1] * q[3] - q[0] * q[2];  //to normalize these terms, multiply each by 2.0
+  vy = q[0] * q[1] + q[2] * q[3];
+  vz = q[0] * q[0] - 0.5f + q[3] * q[3];
+
+  // Error is cross product between estimated and measured direction of gravity in body frame
+  // (half the actual magnitude)
+  ex = (ay * vz - az * vy);
+  ey = (az * vx - ax * vz);
+  ez = (ax * vy - ay * vx);
+
+  // Compute and apply to gyro term the integral feedback, if enabled
+  if (Ki > 0.0f) {
+    ix += Ki * ex * deltat;  // integral error scaled by Ki
+    iy += Ki * ey * deltat;
+    iz += Ki * ez * deltat;
+    gx += ix;  // apply integral feedback
+    gy += iy;
+    gz += iz;
+  }
+
+  // Apply proportional feedback to gyro term
+  gx += Kp * ex;
+  gy += Kp * ey;
+  gz += Kp * ez;
+
+  // Integrate rate of change of quaternion, q cross gyro term
+  deltat = 0.5 * deltat;
+  gx *= deltat;   // pre-multiply common factors
+  gy *= deltat;
+  gz *= deltat;
+  qa = q[0];
+  qb = q[1];
+  qc = q[2];
+  q[0] += (-qb * gx - qc * gy - q[3] * gz);
+  q[1] += (qa * gx + qc * gz - q[3] * gy);
+  q[2] += (qa * gy - qb * gz + q[3] * gx);
+  q[3] += (qa * gz + qb * gy - qc * gx);
+
+  // renormalise quaternion
+  recipNorm = 1.0 / sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
+  q[0] = q[0] * recipNorm;
+  q[1] = q[1] * recipNorm;
+  q[2] = q[2] * recipNorm;
+  q[3] = q[3] * recipNorm;
 }
--- src/inercial_unit/inercial_unit_A/gain_roll_pitch_yaw.h
+++ src/inercial_unit/inercial_unit_A/gain_roll_pitch_yaw.h
@@ -3,6 +3,8 @@
 #define GAIN_ROLL_PITCH_YAW_H
 
 #include "ICM_20948.h"
+#include <Wire.h>
+#include "SparkFun_ISM330DHCX.h"
 
 #define SERIAL_PORT Serial
 #define SPI_PORT SPI
@@ -22,8 +24,18 @@ extern double yawOffset;
 
 void initializeSensor();
 
+typedef enum {
+    SENSOR_ICM20948,
+    SENSOR_ISM330DHCX,
+    SENSOR_UNKNOWN
+} SensorType;
+
+extern SensorType detectedSensor;
+
 // Reads data from sensor and stores them in RollPitchYaw structure
 // @return pointer to RollPitchYaw structure
 RollPitchYaw* readFromSensor();
+
+void Mahony_update(float ax, float ay, float az, float gx, float gy, float gz, float deltat);
 
 #endif // GAIN_ROLL_PITCH_YAW_H
